06ed71521810f3005082b908e22a1d4c6c4418d4
AdeleM-prog <12043@holbertonstudents.com>
2025-12-17 14:47:53 +0000

    crea concept 1-getline

--- 1-getline.c
+++ 1-getline.c
@@ -5,3 +5,24 @@
  * @n: nb of bytes allocated to the buffer
  * Return: Always 0.
  */
+
+int main(void)
+{
+char *line = NULL;
+size_t bufsize = 0;
+ssize_t len = 0;
+
+while (1)
+{
+printf("$ ");
+len = getline(&line, &bufsize, stdin);
+
+if (len == -1)
+break;
+
+printf("%s", line);
+}
+printf("\n");
+free(line);
+return (0);
+}
