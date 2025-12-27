f3d47fd28636610744ddc55aac07afbb967f8824
AdeleM-prog <12043@holbertonstudents.com>
2025-12-27 11:06:43 +0000

    creation super_

---
+++ super_simple_shell.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv, char **envp)
+{
+char *delim = " \t\n";
+char *token;
+char *argv_cmd[2];
+char *line = NULL;
+size_t bufsize = 0;
+ssize_t len = 0;
+pid_t pid;
+
+while (1)
+{
+printf("$ ");
+len = getline(&line, &bufsize, stdin);
+
+if (len == -1)
+break;
+
+token = strtok(line, delim);
+argv_cmd[0] = token;
+argv_cmd[1] = NULL;
+
+if (token != NULL)
+{
+pid = fork();
+if (pid < 0)
+return (-1);
+
+if (pid == 0)
+{
+execve(token, argv_cmd, envp);
+perror("execve");
+exit(1);
+}
+
+if (pid > 0)
+wait(NULL);
+}
+}
+
+printf("\n");
+free(line);
+return (0);
+
+}
