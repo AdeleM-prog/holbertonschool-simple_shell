4c44b0ccc1b85741558d2028e91ae476f2015b30
AdeleM-prog <12043@holbertonstudents.com>
2026-01-07 19:49:15 +0000

    crea path.c + modif execute.c + shell.h

--- execute.c
+++ execute.c
@@ -9,28 +9,32 @@ void execute_cmd(char **args, char *prog_name, char **envp)
 {
 	pid_t pid;
 	int status;
-	char *cmd;
+	char *path;
 
-	cmd = args[0];
-
-	if (access(cmd, F_OK | X_OK) == -1)
+	path = search_path(args[0], envp);
+	if (path == NULL)
 	{
-		fprintf(stderr, "%s: 1: %s: not found\n", prog_name, cmd);
+		fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
 		return;
 	}
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(cmd, args, envp) == -1)
+		if (execve(path, args, envp) == -1)
+		{
+			free(path);
 			exit(127);
+		}
 	}
 	else if (pid > 0)
 	{
 		wait(&status);
+		free(path);
 	}
 	else
 	{
 		perror("fork");
+		free(path);
 	}
 }
